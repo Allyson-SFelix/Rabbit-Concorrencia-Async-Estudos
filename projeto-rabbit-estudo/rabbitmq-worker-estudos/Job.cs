8371db9c7e05f4efffb6f9bdc03f82358339fd21
Allyson-SFelix <allysonstudy2021@gmail.com>
2026-09-09 20:33:08 -0300

    feat: adicionado modulo de backgroundservice

---
+++ projeto-rabbit-estudo/rabbitmq-worker-estudos/Job.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace rabbitMQaprendendo
+{
+
+	public class Job
+	{
+		public string conteudo { get; private set; }
+		public int id { get; private set; }
+		public DateTime data { get; private set; }
+		public bool executada { get; private set; }
+
+		public Job(string conteudo, int id)
+		{
+			this.conteudo = conteudo;
+			this.id = id;
+			if (id % 2 == 0) 
+			{ 
+				this.data = DateTime.Now.AddSeconds(-(id*10));
+			}
+			else
+			{
+                this.data = DateTime.Now.AddSeconds(id*10);
+            }
+            this.executada = false;
+		}
+
+		public void StatusExecutar()
+		{
+			this.executada = true;
+		}
+		
+		public override string ToString()
+		{
+			return "[conteudo: " + this.conteudo + "]\n[id: "+this.id+"]\n";
+		}
+
+		public static List<Job> CriandoSituacao()
+		{
+            List<Job> listaJob = new List<Job>();
+            for (int i = 0; i < 10; i++)
+            {
+                listaJob.Add(new Job("t" + i, i));
+            }
+
+            //foreach (var job in listaJob)
+            //{
+           //     Console.WriteLine(job.ToString());
+           // }
+
+            return listaJob;
+        }
+	}
+}
---
+++ projeto-rabbit-estudo/rabbitmq-worker-estudos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using rabbitMQaprendendo;
+
+namespace rabbitMQaprendendo
+{
+    internal class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            // recebendo chamada da API
+            List<Job> listaJob = Job.CriandoSituacao();
+            Console.WriteLine("[PROGRAM] CRIANDO JOBS");
+            await Teste(listaJob);
+
+            Console.WriteLine("[PROGRAM] PÓS ESCALONAR");
+
+            Console.ReadLine();
+        }
+
+        public static async Task Teste(List<Job> listaJob)
+        {
+            Console.WriteLine("[PROGRAM] ENTRANDO NO TESTE");
+
+
+            Scheduler scheduler = new Scheduler(); //configurado como singleton no program.cs do ASP.NET Core
+
+            _ = scheduler.Escalonar(); //deixa rodar sem precisar esperar o resultado
+            
+            foreach (Job job in listaJob)
+            {
+                scheduler.AddJob(job);
+            }
+
+
+
+            Console.WriteLine("[PROGRAM] SAINDO DO TESTE");
+        }
+    }
+}
